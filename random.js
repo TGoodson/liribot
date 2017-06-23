30ea873747c01a51327659b073b93573d3d00a70
Trevon Goodson <treygoodie@gmail.com>
2017-06-23 13:28:20 -0700

    all files tested
    uploaded

---
+++ random.js
@@ -0,0 +1,42 @@
+exports.commands = [
+	{
+		comm: 'spotify-this-song',
+		argu: 'hot in here'
+	},
+	{
+		comm: 'spotify-this-song',
+		argu: 'Eye in the Sky'
+	},
+	{
+		comm: 'spotify-this-song',
+		argu: 'hot line bling'
+	},
+	{
+		comm: 'movie-this',
+		argu: 'Blade Runner'
+	},
+	{
+		comm: 'movie-this',
+		argu: 'Godzilla'
+	},
+	{
+		comm: 'movie-this',
+		argu: 'King Kong'
+	},
+	{
+		comm: 'spotify-this-song',
+		argu: 'all eyes on me'
+	},
+	{
+		comm: 'spotify-this-song',
+		argu: "lemonaid"
+	},
+	{
+		comm: 'movie-this',
+		argu: 'Stick It'
+	},
+	{
+		comm: 'movie-this',
+		argu: 'Dumbo'
+	}
+];
